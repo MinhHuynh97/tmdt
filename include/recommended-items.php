a0f4dab573fab887131fa48c997131963d31c4bd
Tran Bao <bao.tran133@hcmut.edu.vn>
2022-03-19 07:01:25 +0700

    add recommended-items-carousel

--- include/recommended-items.php
+++ include/recommended-items.php
@@ -1,22 +1,62 @@
-<div class="col-sm-9 padding-right">
-					<div class="features_items">
-						<h2 class="title text-center">Features Items</h2>
-						<?php require_once("get-features-items.php") ?>
-					</div>
-					<div style="text-align:right; margin-right:20px; margin-bottom: 20px">
-						<a href="?quanly=product" class="btn btn-warning" role="button">See more</a>
-					</div>
-					<div class="category-tab"><!--category-tab-->
-						<?php	require_once("category-tab.php")?>	
-					</div><!--/category-tab-->
-					
-					<div class="recommended_items"><!--recommended_items-->
+<?php   
+    //include_once('../database/connect.php');
+    $query = "select * from clothes where 1";
+	$result = mysqli_query($con, $query);
+    $group_item='';
+    $item_list=array();
+    $i=0;$j=0;
+    while($row = mysqli_fetch_array($result)){
+        $temp='<div class="col-sm-4">
+                    <div class="product-image-wrapper">
+                        <div class="single-products">
+                            <div class="productinfo text-center">
+                                <img src="'.$row["img"].'" alt="" />
+                                <h2>'.$row["price"].'</h2>
+                                <p>'.$row["title"].'</p>
+                                <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                            </div>                    
+                        </div>
+                    </div>
+                </div>';
+        array_push($item_list,$temp);        
+    }
+    $item_list=array_reverse($item_list);
+    while(count($item_list)!=0){        
+        if($i==0){
+            $group_item.='<div class="item active">';                      
+        }
+        else{
+            $group_item.='<div class="item">';
+        }
+        for($j=0;$j<=2;$j++){
+            $temp=array_pop($item_list);
+            $group_item.=$temp;
+        }
+        $group_item.='</div>';       
+        $i++;
+        
+    }
+       
+    
+?>
+<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <?php echo $group_item; ?>
+    </div>
+    <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
+	    <i class="fa fa-angle-left"></i></a>
+    <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
+	    <i class="fa fa-angle-right"></i></a>	
+</div>
+
+
+
+
+<!--<div class="recommended_items">recommended_items
 						<h2 class="title text-center">recommended items</h2>
-						<?php	require_once("recommended-items.php")?>	
-						<!--<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">							
+						
+						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">							
 							<div class="carousel-inner">
-															
-									
 								<div class="item active">	
 									<div class="col-sm-4">
 										<div class="product-image-wrapper">
@@ -32,6 +72,19 @@
 										</div>
 									</div>
 									<div class="col-sm-4">
+										<div class="product-image-wrapper">
+											<div class="single-products">
+												<div class="productinfo text-center">
+													<img src="images/home/recommend3.jpg" alt="" />
+													<h2>$56</h2>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+												</div>
+												
+											</div>
+										</div>
+									</div>
+                                    <div class="col-sm-4">
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
@@ -93,8 +146,6 @@
 							  <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
 								<i class="fa fa-angle-right"></i>
 							  </a>			
-						</div>-->
+						</div>
 
-					</div><!--/recommended_items-->
-					
-				</div>
+					</div>recommended_items-->
