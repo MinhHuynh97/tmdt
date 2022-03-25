c3bf3950600b5dde81c94471052430b8aebba6ff
hienvv17 <hienvv17@gmail.com>
2022-03-26 01:24:42 +0700

    update add product to cart

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     // showDataGioHang();
-	
-    function showDataGioHang(){
+
+    function showDataGioHang() {
         // $.ajax({
         //     url:'./giohang_show.php',
         //     type:'POST',
@@ -11,11 +11,11 @@ $(document).ready(function(){
         //         test1:1
         //     },
         //     success:function(res){
-                
+
         //         $('#cart_id').html(res);
-                
-            
-            
+
+
+
         //     }
         // })
     }
@@ -27,51 +27,88 @@ $(document).ready(function(){
     //             test2:1
     //         },
     //         success:function(res){
-                
+
     //             $('#total_cart').html(res);
-                
-            
-            
+
+
+
     //         }
     //     })
     // }
     // $(document).on('input','#soluong_item_giohang',function(){
-	// 	var id_giohang=$(this).attr('id_slgh');
-	// 	var sl_gh=$(this).val();
-	// 	$.ajax({
-	// 			url:'./giohang_sl.php',
-	// 			type:'POST',
-	// 			data:{
-                    
-	// 				id_giohang:id_giohang,
-	// 				sl_gh:sl_gh
-	// 			},
-	// 			success:function(res){
-					
-	// 				showDataGioHang();
-	// 				showTotal()
-	// 			}
-	// 		})
-	// })
-	
+    // 	var id_giohang=$(this).attr('id_slgh');
+    // 	var sl_gh=$(this).val();
+    // 	$.ajax({
+    // 			url:'./giohang_sl.php',
+    // 			type:'POST',
+    // 			data:{
+
+    // 				id_giohang:id_giohang,
+    // 				sl_gh:sl_gh
+    // 			},
+    // 			success:function(res){
+
+    // 				showDataGioHang();
+    // 				showTotal()
+    // 			}
+    // 		})
+    // })
+
     // $(document).on('click','.update',function(){
-	// 	var code_amount=document.getElementById("code_amount").value;
+    // 	var code_amount=document.getElementById("code_amount").value;
     //     var code_gift=document.getElementById("code_gift").value;
-	// 	$.ajax({
-	// 			url:'./giohang_total.php',
-	// 			type:'POST',
-	// 			data:{
+    // 	$.ajax({
+    // 			url:'./giohang_total.php',
+    // 			type:'POST',
+    // 			data:{
     //                 test2:1,
     //                 code_amount:code_amount,
-	// 				code_gift:code_gift
-	// 			},
-	// 			success:function(res){
-					
-	// 				showDataGioHang();
+    // 				code_gift:code_gift
+    // 			},
+    // 			success:function(res){
+
+    // 				showDataGioHang();
     //                 // $('#total_cart').html(res);
-					
-	// 			}
-	// 		})
-	// })
+
+    // 			}
+    // 		})
+    // })
+    $(document).on('click', '#add-to-cart', function () {
+        var user_id = Number($('#session-value').val());
+        var id = $(this).attr('product-id');
+        var input_quantity = $('#input-quantity').val();
+        var remain_quantity = Number($(this).attr('remain-quantity'));
+        var num_item_on_cart = Number($(this).attr('num-item-on-cart'));
+        if (user_id == '') { alert('Please Sign In First!\n If You Do Not Have Account, Please Sign Up!'); }
+        else if ((input_quantity != undefined) && (Number(input_quantity) > remain_quantity)) {
+            alert("You choose too must item ");
+        }
+        else if ((input_quantity != undefined) && (input_quantity == '' || input_quantity == 0)) {
+            alert('Please choose at least one item to add to your cart!');
+        }
+        else {
+            var quantity = 1;
+            if (input_quantity == undefined) { quantity = 1; }
+            else { quantity = Number(input_quantity); }
+            if (num_item_on_cart != 0 && ((num_item_on_cart + quantity) > remain_quantity)) { alert('There is ' + num_item_on_cart + ' item on your cart\n' + 'There is ' + remain_quantity + ' item  on stock.\nYou choose to must item') }
+            else {
+                var new_quantity = quantity + num_item_on_cart;
+                $(this).attr('num-item-on-cart', new_quantity);
+                $.ajax({
+                    url: 'include/add-to-cart.php',
+                    method: 'POST',
+                    data: { id: id, quantity: quantity, user_id: user_id, remain_quantity: remain_quantity },
+
+                    success: function (data) {
+
+                        //alert('Insert đã thành công ' + id + " quantity is " + quantity + ' remain ' + remain_quantity + ' item ' + new_quantity);
+                        alert('This item has beemn added to your cart\n' + new_quantity + ' of this is on your cart now!');
+                    }
+                })
+            }
+        }
+
+
+    })
 
 })
