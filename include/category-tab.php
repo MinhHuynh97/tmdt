85d5c155558ab55960a7958e64ad8c072609c1ad
hienvv17 <hienvv17@gmail.com>
2022-03-31 19:51:46 +0700

    add link detail to product detail category tab, fix warning

--- include/category-tab.php
+++ include/category-tab.php
@@ -30,7 +30,7 @@ while ($row = mysqli_fetch_array($result)) {
         $query = "SELECT cart_details.quantity, clothes.quantity 
 						as cart_check FROM `cart_details`  
 						inner join `carts` on carts.id=cart_details.cart_id 
-						inner join `clothes` on cart_details.clothing_id=clothes.id WHERE cart_details.clothing_id = '$id' and carts.user_id = $session_value";
+						inner join `clothes` on cart_details.clothing_id=clothes.id WHERE cart_details.clothing_id = '$id' and carts.user_id = '$session_value'";
         $check_cart = mysqli_query($con, $query);
 
         if ($check_cart->num_rows != 0) {
@@ -43,7 +43,7 @@ while ($row = mysqli_fetch_array($result)) {
                 <div class="product-image-wrapper">
                     <div class="single-products">
                         <div class="productinfo text-center">
-                            <img src="' . $sub_row["img"] . '" alt="" />
+                        <a href="?quanly=product&id='.$sub_row['id'].'"><img src="' . $sub_row["img"] . '" alt="" /></a>
                             <h2>' . $sub_row["price"] . '</h2>
                             <p>' . $sub_row["title"] . '</p>
                             <button id="add-to-cart" class="btn btn-default add-to-cart"  product-id="' . $sub_row['id'] . '" remain-quantity="' . $sub_row['quantity'] . '" num-item-on-cart="' . $num_item_on_cart . '">
@@ -53,13 +53,15 @@ while ($row = mysqli_fetch_array($result)) {
                 </div>            
         </div>';
         }
+        
         else{
             $tab_content .= '
             <div class="col-sm-3">
                 <div class="product-image-wrapper">
                     <div class="single-products">
                         <div class="productinfo text-center">
-                            <img src="' . $sub_row["img"] . '" alt="" />
+                            <a href="?quanly=product&id='.$sub_row['id'].'"><img src="' . $sub_row["img"] . '" alt="" /></a>
+                            
                             <h2>' . $sub_row["price"] . '</h2>
                             <p>' . $sub_row["title"] . '</p>
                             <button class="btn btn-default add-to-cart"><i class="fa-solid fa-sync fa-spin"></i>Out of Stock</button>
